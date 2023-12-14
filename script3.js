905f3e07337670bfb2a05fed921849acaf6c8635
veerverma828 <138509541+veerverma828@users.noreply.github.com>
2023-12-15 05:03:00 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,13 @@
+function goToFirstPage() {
+    // Redirect to the first page
+    window.location.href = 'index.html';
+}
+
+//prints page
+
+function printPage() {
+    window.print();
+}
+
+
+
---
+++ test.js
@@ -0,0 +1,4 @@
+// Initialization for ES Users
+import { Carousel, initMDB } from "mdb-ui-kit";
+
+initMDB({ Carousel });
